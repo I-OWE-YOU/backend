463bc4a39d878ef3903270a914582f4ee64c6293
Rick van den Bosch <rickvdbosch@outlook.com>
2020-03-16 11:56:46 +0100

    Added company class

---
+++ IOU.Common.Models/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace IOU.Common.Models
+{
+    public class Company
+    {
+        public string CompanyName { get; set; }
+
+        public string ContactFirstName { get; set; }
+
+        public string  ContactLastName { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
