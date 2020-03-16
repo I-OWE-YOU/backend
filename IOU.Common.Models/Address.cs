9ed22aacda1c5ea8bcbe370e8518c6ba68d8d6c1
Rick van den Bosch <rickvdbosch@outlook.com>
2020-03-16 11:56:35 +0100

    Added address

---
+++ IOU.Common.Models/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace IOU.Common.Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+
+        public int HouseNumber { get; set; }
+
+        public string HouseNumberAddition { get; set; }
+
+        public string Zipcode { get; set; }
+
+        public string City { get; set; }
+    }
+}
