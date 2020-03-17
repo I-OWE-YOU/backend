2e1a2c073931919556bb23f6253e9dc0f092e2a6
Rick van den Bosch <rickvdbosch@outlook.com>
2020-03-17 22:03:00 +0100

    Added Common project and some Azure specific supporting classes

---
+++ IOS.Common/Azure/BaseStorageRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.WindowsAzure.Storage;
+
+namespace IOU.Common.Azure
+{
+    public abstract class BaseStorageRepository
+    {
+        #region Fields
+
+        protected CloudStorageAccount _storageAccount;
+
+        #endregion
+
+        #region Constructors
+
+        public BaseStorageRepository(string connectionString)
+        {
+            _storageAccount = CloudStorageAccount.Parse(connectionString);
+        }
+
+        #endregion
+    }
+}
---
+++ IOS.Common/Azure/BlobStorageRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace IOU.Common.Azure
+{
+    public class BlobStorageRepository : BaseStorageRepository
+    {
+        #region Fields
+
+        private CloudBlobClient _blobClient;
+
+        #endregion
+
+        #region Constants
+
+        private const string FILES_CONTAINER = "files";
+
+        #endregion
+
+        #region Constructors
+
+        public BlobStorageRepository(string connectionString) : base(connectionString)
+        {
+            _blobClient = _storageAccount.CreateCloudBlobClient();
+        }
+
+        #endregion
+
+        public async Task<string> GetFileContentAsync(string fileName)
+        {
+            var container = _blobClient.GetContainerReference(FILES_CONTAINER);
+            var blob = container.GetBlockBlobReference(fileName);
+
+            return await blob.DownloadTextAsync();
+        }
+    }
+}
